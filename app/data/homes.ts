80dd80e4a224580f20e788454de595b285e19009
zf-2022 <113913728+ZaFuh@users.noreply.github.com>
2025-03-18 02:56:17 +0400

    added drop downt o home menu on side bar

---
+++ app/data/homes.ts
@@ -0,0 +1,20 @@
+import { Home } from "../types/home";
+
+export const homes: Home[] = [
+  {
+    id: "1",
+    name: "Greenwood Residence",
+    address: "123 Eco Street, London",
+    image: "https://cdn.builder.io/api/v1/image/assets/e97f4b049aa04c0fb59c904d1d337327/14a29b276f9fa7247203d23e3c249148349565fd41cf0e78a817dca439ea1f7a",
+    dwellers: [],
+    devices: []
+  },
+  {
+    id: "2",
+    name: "City Apartment",
+    address: "456 Urban Ave, London",
+    image: "https://cdn.builder.io/api/v1/image/assets/e97f4b049aa04c0fb59c904d1d337327/14a29b276f9fa7247203d23e3c249148349565fd41cf0e78a817dca439ea1f7a",
+    dwellers: [],
+    devices: []
+  }
+];
