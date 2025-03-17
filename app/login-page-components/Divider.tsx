201d92de604990724b9d790c764ee37d8546a23b
zf-2022 <113913728+ZaFuh@users.noreply.github.com>
2025-03-17 14:01:07 +0400

    fixed login page and added mobile version

--- app/login-page-components/Divider.tsx
+++ app/login-page-components/Divider.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const Divider: React.FC = () => {
   return (
-    <div className="flex gap-5 justify-between items-center mt-16 text-sm max-md:mt-10">
-      <div className="shrink-0 self-stretch my-auto h-0.5 border border-black border-solid w-[186px]" />
-      <span className="self-stretch">OR</span>
-      <div className="shrink-0 self-stretch my-auto h-0.5 border border-black border-solid w-[186px]" />
+    <div className="flex gap-5 justify-center items-center mt-16 text-sm max-md:mt-10 w-full">
+      <div className="grow h-0.5 border border-black border-solid max-w-[186px]" />
+      <span>OR</span>
+      <div className="grow h-0.5 border border-black border-solid max-w-[186px]" />
     </div>
   );
 };
--- app/login-page-components/Login.tsx
+++ app/login-page-components/Login.tsx
@@ -5,13 +5,13 @@ import { LoginForm } from "./LoginForm";
 
 function Login() {
   return (
-    <main className="flex overflow-hidden flex-col pt-6 pr-9 pb-16 pl-20 bg-amber-50 max-md:px-5">
-      <div className="self-center ml-4 w-full max-w-[1201px] max-md:max-w-full">
+    <main className="flex h-screen w-full overflow-hidden flex-col bg-amber-50 md:overflow-hidden md:px-20 max-md:h-full max-md:overflow-auto">
+      <div className="self-center w-full max-w-[1201px] px-4 py-6">
         <div className="flex gap-5 max-md:flex-col">
-          <section className="w-6/12 max-md:ml-0 max-md:w-full">
+          <section className="w-6/12 max-md:w-full">
             <HeroBanner />
           </section>
-          <section className="ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+          <section className="w-6/12 max-md:w-full">
             <LoginForm />
           </section>
         </div>
